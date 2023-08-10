81a474167089c48531f64ed06912ff468f855a8c
Alignment Lab AI <autometa@alignmentlab.ai>
2023-08-10 16:27:17 -0500

    Update api.py

--- api.py
+++ api.py
@@ -1,25 +1,51 @@
-
 import os
 import json
 import asyncio
 import aiohttp
-
-BATCH_SIZE = 50
-MAX_RETRIES = 5
-RETRY_DELAY = 2
+import time
+import numpy as np
 
 class OpenAI_API:
+    MAX_RETRIES = 5
+    RETRY_DELAY = 2
+    THROUGHPUT_SAMPLES = 10
+
     def __init__(self):
         self.session = None
+        self.throughput = np.zeros(self.THROUGHPUT_SAMPLES)
+        self.throughput_index = 0
 
     async def send_prompt(self, prompt, index, results_queue, inputs_queue):
-        base_url = os.environ['OPENAI_API_BASE']
+        base_url = os.getenv('OPENAI_API_BASE')
         url = f"{base_url}/chat/completions"
-        headers = {
+        headers = self.get_headers()
+        data = self.get_data(prompt)
+        for retry in range(self.MAX_RETRIES):
+            try:
+                start_time = time.time()
+                result, response_text = await self.post_request(url, headers, data)
+                end_time = time.time()
+                self.update_throughput(end_time - start_time)
+                if self.is_special_response(response_text):
+                    continue
+                else:
+                    await self.handle_normal_response(index, result, prompt, results_queue, inputs_queue)
+                break
+            except Exception as e:
+                print(f'Error sending prompt {index} (retry {retry + 1}):', e)
+                if retry == self.MAX_RETRIES - 1:
+                    print(f'Failed to process prompt {index} after {self.MAX_RETRIES} retries.')
+                    return (index, None)
+                await asyncio.sleep(self.RETRY_DELAY ** (retry + 1))
+
+    def get_headers(self):
+        return {
             'Content-Type': 'application/json',
-            'Authorization': f"Bearer {os.environ['OPENAI_API_KEY']}"
+            'Authorization': f"Bearer {os.getenv('OPENAI_API_KEY')}"
         }
-        data = {
+
+    def get_data(self, prompt):
+        return {
             "model": "gpt-3.5-turbo",
             "messages": [
                 {
@@ -28,41 +54,38 @@ class OpenAI_API:
                 }
             ]
         }
-        for retry in range(MAX_RETRIES):
-            try:
-                async with self.session.post(url, headers=headers, json=data) as response:
-                    result = await response.json()
-                    response_text = json.dumps(result)
 
-                    if any(phrase in response_text for phrase in ["%assistant%", "%I'm sorry%", "% AI %", "%language model%", "%I cannot%", "%September 2021%"]):
-                        print(f"Special response detected for prompt {index}")
-                        await results_queue.put((index, result))
-                        await inputs_queue.put((index, prompt))
-                        return (index, result)
+    async def post_request(self, url, headers, data):
+        async with self.session.post(url, headers=headers, json=data) as response:
+            result = await response.json()
+            return result, json.dumps(result)
 
-                    print(f'Model output for prompt {index}:', response_text)
-                    await results_queue.put((index, result))
-                    await inputs_queue.put((index, prompt))
-                    return (index, result)
-            except Exception as e:
-                print(f'Error sending prompt {index} (retry {retry + 1}):', e)
-                await asyncio.sleep(RETRY_DELAY ** (retry + 1))
-        print(f'Failed to process prompt {index} after {MAX_RETRIES} retries.')
-        return (index, None)
+    def is_special_response(self, response_text):
+        return any(phrase in response_text for phrase in ["%assistant%", "%I'm sorry%", "% AI %", "%language model%", "%I cannot%", "%September 2021%"])
+
+    async def handle_normal_response(self, index, result, prompt, results_queue, inputs_queue):
+        print(f'Model output for prompt {index}:', json.dumps(result))
+        await results_queue.put((index, result))
+        await inputs_queue.put((index, prompt))
+        return (index, result)
+
+    def update_throughput(self, elapsed_time):
+        self.throughput[self.throughput_index] = 1 / elapsed_time
+        self.throughput_index = (self.throughput_index + 1) % self.THROUGHPUT_SAMPLES
 
     async def process_prompts(self, prompt_generator):
         results_queue = asyncio.Queue()  # Queue to hold results
         inputs_queue = asyncio.Queue()  # Queue to hold indexed inputs
 
         # Start coroutines to write results and inputs to files
-        asyncio.create_task(self.write_to_file(results_queue, 'complete.json'))
-        asyncio.create_task(self.write_to_file(inputs_queue, 'indexed_inputs.json'))
+        asyncio.create_task(self.write_to_file(results_queue, 'postapi/complete.jsonl'))
+        asyncio.create_task(self.write_to_file(inputs_queue, 'postapi/indexed_inputs.jsonl'))
 
         async with aiohttp.ClientSession() as self.session:
             batch = []
             for index, prompt in enumerate(prompt_generator):
                 batch.append((index, prompt))
-                if len(batch) >= BATCH_SIZE:
+                if len(batch) >= self.THROUGHPUT_SAMPLES:
                     await self.process_batch(batch, results_queue, inputs_queue)
                     batch = []
             if batch:
@@ -72,36 +95,29 @@ class OpenAI_API:
         await results_queue.put(None)
         await inputs_queue.put(None)
 
-    async def write_to_file(self, queue, file_name, buffer_size=100):
+    async def write_to_file(self, queue, file_name):
         with open(file_name, 'w') as f:
-            f.write("[\n")
-            buffer = []
             while True:
                 item = await queue.get()
-                if item is None and buffer:
-                    f.write(",\n".join(buffer))
+                if item is None:
                     break
-                buffer.append(json.dumps(item))
-                if len(buffer) >= buffer_size:
-                    f.write(",\n".join(buffer))
-                    buffer = []
-            f.write("\n]\n")
+                f.write(json.dumps(item) + "\n")  # Add newline for valid jsonl formatting
 
     async def process_batch(self, batch, results_queue, inputs_queue):
         tasks = [self.send_prompt(prompt, index, results_queue, inputs_queue) for index, prompt in batch]
         return await asyncio.gather(*tasks)
 
 def ask_for_generations():
-    try:
-        num_generations = int(input("Please enter the number of generations you want to perform: "))
-        return num_generations
-    except ValueError:
-        print("Invalid input! Please enter an integer value.")
-        return ask_for_generations()
+    while True:
+        try:
+            num_generations = int(input("Please enter the number of generations you want to perform: "))
+            return num_generations
+        except ValueError:
+            print("Invalid input! Please enter an integer value.")
 
 def prompt_generator(num_generations):
-    with open('prompts.json', 'r') as f:
-        prompts = json.load(f)[:num_generations]
+    with open('preapi/preapi.jsonl', 'r') as f:
+        prompts = [json.loads(line) for line in f][:num_generations]
         for prompt in prompts:
             yield prompt
 
@@ -114,4 +130,8 @@ def main():
         print('An error occurred:', e)
 
 if __name__ == "__main__":
+
     main()
+
+
+
